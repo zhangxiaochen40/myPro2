4c3aa6a8d8fcac3b979e269413c5bc43c1d3e838
zhangxiaochen <zhangxiaochen40@sohu.com>
2018-09-10 19:07:27 +0800

    orglist

--- apps/organization/views.py
+++ apps/organization/views.py
@@ -13,8 +13,29 @@ class OrgListView(View):
     def get(self, request):
 
         org_list = CourseOrg.objects.all()
-        org_count= org_list.count()
+
         city_list = CityDict.objects.all()
+
+        hot_org = CourseOrg.objects.order_by('click_nums')[:3]
+
+        # 城市筛选
+        city_id = request.GET.get('city', '')
+        if city_id:
+            org_list = CourseOrg.objects.filter(city_id=city_id)
+
+        # 机构类别筛选
+        category = request.GET.get('ct','')
+        if category:
+            org_list = CourseOrg.objects.filter(category=category)
+
+        sort = request.GET.get('sort','')
+        if sort:
+            if sort == 'students':
+                org_list = org_list.order_by('-students')
+            elif sort == 'courses':
+                org_list = org_list.order_by('click_nums')
+
+        org_count = org_list.count()
 
         try:
             page = request.GET.get('page', 1)
@@ -25,4 +46,11 @@ class OrgListView(View):
 
         org = p.page(page)
 
-        return render(request, 'org-list.html', {'org_list': org, 'city_list': city_list,'org_count': org_count})
+        return render(request, 'org-list.html', {'org_list': org,
+                                                 'city_list': city_list,
+                                                 'org_count': org_count,
+                                                 'city_id': city_id,
+                                                 'category': category,
+                                                 'hot_org':hot_org,
+                                                 'sort':sort
+                                                 })
