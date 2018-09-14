78af1c6d19af44ff14b59f09a8c02d1064b4af10
zhangxiaochen <zhangxiaochen40@sohu.com>
2018-09-14 08:39:06 +0800

    增加课程详情和评论

--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -1,6 +1,6 @@
 # _*_ coding:utf-8 _*_
 from django.conf.urls import url, include
-from .views import CourseListView, CourseDetailView, CourseInfoView, CourseCommentView, AddCourseComment
+from .views import CourseListView, CourseDetailView, CourseInfoView, CourseCommentView, AddCourseComment, VideoPlayView
 
 urlpatterns = [
     # 课程列表页
@@ -12,5 +12,7 @@ urlpatterns = [
     # 评论
     url(r'^comment/(?P<course_id>\d+)', CourseCommentView.as_view(), name='course_comment'),
     # 添加评论
-    url(r'^add_comment/$', AddCourseComment.as_view(), name='add_comment')
+    url(r'^add_comment/$', AddCourseComment.as_view(), name='add_comment'),
+    # 视频播放
+    url(r'^video/(?P<video_id>\d+)', VideoPlayView.as_view(), name='video_play')
 ]
--- apps/courses/views.py
+++ apps/courses/views.py
@@ -4,8 +4,8 @@ from django.views.generic.base import View
 from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse
 
-from .models import Course, CourseResource
-from operation.models import UserFavorite, CourseComments
+from .models import Course, CourseResource, Video
+from operation.models import UserFavorite, CourseComments, UserCourse
 
 # Create your views here.
 
@@ -84,14 +84,34 @@ class CourseInfoView(View):
     def get(self, request, course_id):
 
         course_info = Course.objects.get(id=int(course_id))
+        course_info.students += 1
+        course_info.save()
         lessons = course_info.get_lesson(request)
+        # 判断用户是否关联了该课程
+        user_course = UserCourse.objects.filter(user=request.user, course=course_info)
+        if not user_course:
+            user_course = UserCourse()
+            user_course.user = request.user
+            user_course.course = course_info
+            user_course.save()
+        # 根据课程获得所有学习过这个课程的用户
+        user_courses = UserCourse.objects.filter(course=course_info)
+        # 获得用户id
+        user_ids = [u_course.user.id for u_course in user_courses]
+        # 根据用户id找到
+        all_user_courses = UserCourse.objects.filter(user_id__in=user_ids)
+        # 取出所有课程id
+        course_ids = [user_couser.course.id for user_couser in all_user_courses]
+        # 获取学过该用户学过其他的所有课程
+        relate_courses = Course.objects.filter(id__in=course_ids).order_by("-click_nums")[:5]
         # 课程资源
         all_resources = CourseResource.objects.filter(course=course_info)
 
         return render(request, 'course-video.html', {
             'course_info': course_info,
             'all_resources': all_resources,
-            'lessons': lessons
+            'lessons': lessons,
+            'relate_courses': relate_courses
         })
 
 
@@ -136,3 +156,37 @@ class AddCourseComment(View):
             return HttpResponse("{'status':'success','msg':'添加成功'}", content_type='application/json')
         else:
             return HttpResponse("{'status':'fail','msg':'添加失败'}", content_type='application.json')
+
+
+class VideoPlayView(View):
+    def get(self, request, video_id):
+        video = Video.objects.get(id=int(video_id))
+        course_info = video.lesson.course
+        # 判断用户是否关联了该课程
+        user_course = UserCourse.objects.filter(user=request.user, course=course_info)
+        lessons = course_info.get_lesson(request)
+        if not user_course:
+            user_course = UserCourse()
+            user_course.user = request.user
+            user_course.course = course_info
+            user_course.save()
+        # 根据课程获得所有学习过这个课程的用户
+        user_courses = UserCourse.objects.filter(course=course_info)
+        # 获得用户id
+        user_ids = [u_course.user.id for u_course in user_courses]
+        # 根据用户id找到
+        all_user_courses = UserCourse.objects.filter(user_id__in=user_ids)
+        # 取出所有课程id
+        course_ids = [user_couser.course.id for user_couser in all_user_courses]
+        # 获取学过该用户学过其他的所有课程
+        relate_courses = Course.objects.filter(id__in=course_ids).order_by("-click_nums")[:5]
+        # 课程资源
+        all_resources = CourseResource.objects.filter(course=course_info)
+
+        return render(request, 'course-play.html', {
+            'course_info': course_info,
+            'all_resources': all_resources,
+            'video': video,
+            'relate_courses': relate_courses,
+            'lessons': lessons
+        })
